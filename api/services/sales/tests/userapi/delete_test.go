c45ee8614a8504520160b7fada016b848cefc487
Adil <addick@me.com>
2025-10-17 17:36:25 +0800

    Enable delete user api test
    - Added delete user api test file
    - Updated user api test file to include new test cases

---
+++ api/services/sales/tests/userapi/delete_test.go
@@ -0,0 +1,74 @@
+package userapi_test
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/warlck/food-flow/app/sdk/apitest"
+	"github.com/warlck/food-flow/app/sdk/errs"
+)
+
+func delete200(sd apitest.SeedData) []apitest.Table {
+	table := []apitest.Table{
+		{
+			Name:       "asuser",
+			URL:        fmt.Sprintf("/v1/users/%s", sd.Users[1].ID),
+			Token:      sd.Users[1].Token,
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusNoContent,
+		},
+		{
+			Name:       "asadmin",
+			URL:        fmt.Sprintf("/v1/users/%s", sd.Admins[1].ID),
+			Token:      sd.Admins[1].Token,
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusNoContent,
+		},
+	}
+
+	return table
+}
+
+func delete401(sd apitest.SeedData) []apitest.Table {
+	table := []apitest.Table{
+		{
+			Name:       "emptytoken",
+			URL:        fmt.Sprintf("/v1/users/%s", sd.Users[0].ID),
+			Token:      "&nbsp;",
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusUnauthorized,
+			GotResp:    &errs.Error{},
+			ExpResp:    errs.Newf(errs.Unauthenticated, "error parsing token: token is malformed: token contains an invalid number of segments"),
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		},
+		{
+			Name:       "badsig",
+			URL:        fmt.Sprintf("/v1/users/%s", sd.Users[0].ID),
+			Token:      sd.Users[0].Token + "A",
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusUnauthorized,
+			GotResp:    &errs.Error{},
+			ExpResp:    errs.Newf(errs.Unauthenticated, "authentication failed: OPA policy evaluation failed for authentication: OPA policy rule \"auth\" not satisfied"),
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		},
+		{
+			Name:       "wronguser",
+			URL:        fmt.Sprintf("/v1/users/%s", sd.Users[0].ID),
+			Token:      sd.Users[2].Token,
+			Method:     http.MethodDelete,
+			StatusCode: http.StatusUnauthorized,
+			GotResp:    &errs.Error{},
+			ExpResp:    errs.Newf(errs.Unauthenticated, "authorize: you are not authorized for that action, claims[[USER]] rule[rule_admin_or_subject]"),
+			CmpFunc: func(got any, exp any) string {
+				return cmp.Diff(got, exp)
+			},
+		},
+	}
+
+	return table
+}
